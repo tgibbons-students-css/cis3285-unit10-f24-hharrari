d91e54c06f6939dcde05f1aecbd44bf1ebf62766
hharrari <148915499+hharrari@users.noreply.github.com>
2024-11-09 20:30:29 -0600

    Learning Activity 10 done

---
+++ SingleResponsibilityPrinciple/AdjustTradeDataProvider.cs
@@ -0,0 +1,37 @@
+﻿using SingleResponsibilityPrinciple.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple
+{
+    public class AdjustTradeDataProvider : ITradeDataProvider
+    {
+        ITradeDataProvider origProvider;
+        private IEnumerable<String> lines;
+
+        AdjustTradeDataProvider(ITradeDataProvider origProvider)
+        {
+            this.origProvider = origProvider;
+        }
+        public IEnumerable<string> GetTradeData()
+        {
+            // calling the or
+            IEnumerable<string> lines = origProvider.GetTradeData();
+
+            List<String> result = new List<string>();
+
+            foreach (String line in lines)
+            {
+                String newLine = line.Replace("GBP", "EUR");
+                result.Add(newLine);
+            }
+            return lines;
+        }
+
+    }
+    
+
+}
